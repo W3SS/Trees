01d567da1599393682d39510927c64c5c7f72c1c
Arthur Maciejewicz <arthur.maciejewicz@gmail.com>
2017-10-17 18:34:46 -0400

    Add logging library and testing utils

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "Classification and Regression Trees"
   :url "https://zintin.io/cart"
   :license {:name "Apache 2"
-            :url ""}
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-csv "2.0.2"]
-                 [walmartlabs/datascope "0.1.1"]
-                 ])
+                 [com.taoensso/timbre "4.10.0"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]
+                                  [walmartlabs/datascope "0.1.1"]]}})

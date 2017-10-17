853ff2d113994200e3204a27457cc54d548625eb
Arthur Maciejewicz <arthur.maciejewicz@gmail.com>
2017-10-17 18:48:23 -0400

    Add tests for axis-aligned rectangles

--- test/cart/tree_test.clj
+++ test/cart/tree_test.clj
@@ -4,7 +4,11 @@
 
 
 (deftest classifies-xor
-         (let [dataframe {:df/count 4
+         (let [;; | o     x
+               ;; |
+               ;; | x     o
+               ;;  ----------
+               dataframe {:df/count 4
                           "x1" {:values [0 0 1 1]
                                 :storage-type :int
                                 :domain-type :discrete}
@@ -19,3 +23,40 @@
            (is (= (t/classify learned-tree {"x1" 0 "x2" 1}) 1))
            (is (= (t/classify learned-tree {"x1" 1 "x2" 0}) 1))
            (is (= (t/classify learned-tree {"x1" 1 "x2" 1}) -1))))
+
+
+(deftest classifies-axis-aligned-rectangles
+  (let [;; | o     x
+        ;; |
+        ;; | o     x
+        ;;  ----------
+        rect1 {:df/count 4
+               "x1" {:values [0 0 1 1]:storage-type :int :domain-type :discrete}
+               "x2" {:values [0 1 0 1] :storage-type :int :domain-type :discrete}
+               "y"  {:values [1 1 -1 -1] :storage-type :int :domain-type :discrete}}
+
+        tree1 (t/learn rect1 #{"x1" "x2"} "y")
+
+        ;; | x     x
+        ;; |
+        ;; | o     o
+        ;;  ----------
+        rect2 {:df/count 4
+               "x1" {:values [0 0 1 1]:storage-type :int :domain-type :discrete}
+               "x2" {:values [0 1 0 1] :storage-type :int :domain-type :discrete}
+               "y"  {:values [1 -1 1 -1] :storage-type :int :domain-type :discrete}}
+
+        tree2 (t/learn rect2 #{"x1" "x2"} "y")]
+
+    (is (= (t/classify tree1 {"x1" 0 "x2" 0}) 1))
+    (is (= (t/classify tree1 {"x1" 0 "x2" 1}) 1))
+    (is (= (t/classify tree1 {"x1" 1 "x2" 0}) -1))
+    (is (= (t/classify tree1 {"x1" 1 "x2" 1}) -1))
+
+
+    (is (= (t/classify tree2 {"x1" 0 "x2" 0}) 1))
+    (is (= (t/classify tree2 {"x1" 0 "x2" 1}) -1))
+    (is (= (t/classify tree2 {"x1" 1 "x2" 0}) 1))
+    (is (= (t/classify tree2 {"x1" 1 "x2" 1}) -1))
+
+    ))

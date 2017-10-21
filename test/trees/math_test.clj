ec1128b03513006a9a5d0098df4db6d8366e4e6a
Arthur Maciejewicz <arthur.maciejewicz@gmail.com>
2017-10-21 11:06:46 -0400

    More tests

--- test/trees/math_test.clj
+++ test/trees/math_test.clj
@@ -3,15 +3,30 @@
             [trees.math :as m]))
 
 
+(deftest find-maximal-values
+  (is (= [-2 4] (m/find-maximal-values #(* % %) [-2 -1 0 1 2])))
+  (is (= [3 3] (m/find-maximal-values identity [0 1 2 3]))))
+
+
+(deftest find-minimal-values
+  (is (= [0 0] (m/find-minimal-values #(* % %) [-2 -1 0 1 2])))
+  (is (= [0 0] (m/find-minimal-values identity [0 1 2 3]))))
+
+
 (deftest find-min
          (let [xs [-2 -1 0 1 2]
                f #(* % %)]
-           (is (= (m/min f xs) 0))
-           (is (= (m/arg-min f xs) 0))))
+           (is (= 0 (m/min f xs)))
+           (is (= 0 (m/arg-min f xs)))))
 
 
 (deftest find-max
   (let [xs [-2 -1 0 1 2]
         f identity]
-    (is (= (m/max f xs) 2))
-    (is (= (m/arg-max f xs) 2))))
+    (is (= 2 (m/max f xs)))
+    (is (= 2 (m/arg-max f xs)))))
+
+
+(deftest sum
+  (is (= 6 (m/sum [1 2 3])))
+  (is (= 6.0 (m/sum [1.0 2.0 3.0]))))
--- test/trees/measures_test.clj
+++ test/trees/measures_test.clj
@@ -1,1 +1,17 @@
-(ns trees.measures-test)
+(ns trees.measures-test
+  (:require [clojure.test :refer :all]
+            [trees.measures :as m]))
+
+
+(deftest fall-into-node-with-class-j-probability
+
+  (let [node  {:class-counts {:pos 10 :neg 100}}
+        prior {:pos 0.50 :neg 0.50}
+        class-distribution {:pos 1000 :neg 1000}
+
+        pos-prob (m/fall-into-node-with-class-j-probability node :pos prior class-distribution)
+        neg-prob (m/fall-into-node-with-class-j-probability node :neg prior class-distribution)]
+
+    (is (> neg-prob pos-prob))
+    (is (= 0.05 neg-prob))
+    (is (= 0.005 pos-prob))))
